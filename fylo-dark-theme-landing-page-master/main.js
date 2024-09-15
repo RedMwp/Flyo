e5ff54e508b7d8a6760ac4088b7ab25d60aa8fe5
redmwp <redmwp1@gmail.com>
2024-09-15 13:33:58 +0200

    update

--- fylo-dark-theme-landing-page-master/main.js
+++ fylo-dark-theme-landing-page-master/main.js
@@ -1,6 +1,62 @@
-gsap.from('.illustration',{
+
+let btn = document.querySelector('.hamburg');
+let input = document.querySelector('input');
+let form = document.querySelector('form');
+
+
+//form error
+  form.addEventListener('submit',(e)=>{
+  e.preventDefault()
+
+  if(input.value === ''){
+    input.style.border = '1px solid red';
+    input.placeholder =' enter correct email'
+  } else {
+    input.style.border = 'none'
+    input.value = ''
+    input.placeholder = 'email sent'
+  }
+})
+//nav animations
+let open = false;
+let tl = gsap.timeline()
+
+btn.onclick = ()=>{
+if(open === false){
+
+tl.to('.hamburg',{
+     height:10,
+      duration:.4
+    })
+      .to('.main-nav-list',{
+  height:'auto',
+  duration:.5,
+  ease:'power'
+})
+  .from('.main-nav-list li',{
   opacity:0,
-  duration:4,
-  rotate:'30deg',
-  skewY:70
-  });
+  duration:.8,
+  y:10,
+  ease:'power',
+  skewY:50,
+  stagger:{
+    amount:.5,
+    each:.5,
+    from:2
+  }
+})
+
+open = true;
+  } else{
+  tl.to('.hamburg',{
+      height:16
+    }).to(".main-nav-list",{
+      height:0,
+      duration:.5
+    })
+    open = false;
+  }}
+//scroll animations
+//
+
+
---
+++ fylo-dark-theme-landing-page-master/scrollAnimations.js
@@ -0,0 +1,6 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('.cta',{
+  opacity:0,
+  duration:5
+})
